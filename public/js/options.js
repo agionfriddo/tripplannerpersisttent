e3624501c2dbe6951d663a856e8ee81356e7240c
rjisoo <jisoo.cc@gmail.com>
2016-10-05 16:48:00 -0400

    options

--- public/js/options.js
+++ public/js/options.js
@@ -11,32 +11,65 @@
 
 $(function(){
 
-  // jQuery selects
-  var $optionsPanel = $('#options-panel');
-  var $hotelSelect = $optionsPanel.find('#hotel-choices');
-  var $restaurantSelect = $optionsPanel.find('#restaurant-choices');
-  var $activitySelect = $optionsPanel.find('#activity-choices');
+	// jQuery selects
+	var $optionsPanel = $('#options-panel');
+	var $hotelSelect = $optionsPanel.find('#hotel-choices');
+	var $restaurantSelect = $optionsPanel.find('#restaurant-choices');
+	var $activitySelect = $optionsPanel.find('#activity-choices');
 
-  // make all the option tags (second arg of `forEach` is a `this` binding)
-  hotels.forEach(makeOption, $hotelSelect);
-  restaurants.forEach(makeOption, $restaurantSelect);
-  activities.forEach(makeOption, $activitySelect);
+	// make all the option tags (second arg of `forEach` is a `this` binding)
+	//hotels.forEach(makeOption, $hotelSelect);
+	//restaurants.forEach(makeOption, $restaurantSelect);
+	//activities.forEach(makeOption, $activitySelect);
 
-  function makeOption (databaseAttraction) {
-    var $option = $('<option></option>') // makes a new option tag
-      .text(databaseAttraction.name)
-      .val(databaseAttraction.id);
-    this.append($option); // add the option to the specific select
-  }
+	function makeOption (databaseAttraction) {
+		var $option = $('<option></option>') // makes a new option tag
+			.text(databaseAttraction.name)
+			.val(databaseAttraction.id);
+		this.append($option); // add the option to the specific select
+	}
 
-  // what to do when the `+` button next to a `select` is clicked
-  $optionsPanel.on('click', 'button[data-action="add"]', function () {
-    var $select = $(this).siblings('select');
-    var type = $select.data('type'); // from HTML data-type attribute
-    var id = $select.find(':selected').val();
-    // get associated attraction and add it to the current day in the trip
-    var attraction = attractionsModule.getByTypeAndId(type, id);
-    tripModule.addToCurrent(attraction);
-  });
+	// create restaurant options
+	$.ajax({
+	  method: 'GET',
+	  url: '/api/restaurants'
+	})
+	.then(function (restaurants) {
+	  restaurants.forEach(makeOption, $restaurantSelect);
+	})
+	.catch(console.error.bind(console));
+
+	// create hotel options
+	$.ajax({
+	  method: 'GET',
+	  url: '/api/hotels'
+	})
+	.then(function (hotels) {
+	  hotels.forEach(makeOption, $hotelSelect);
+	})
+	.catch(console.error.bind(console));
+
+	// create activity options
+	$.ajax({
+	  method: 'GET',
+	  url: '/api/activities'
+	})
+	.then(function (activities) {
+	  activities.forEach(makeOption, $activitySelect);
+	})
+	.catch(console.error.bind(console));
+
+
+
+
+	// what to do when the `+` button next to a `select` is clicked
+	$optionsPanel.on('click', 'button[data-action="add"]', function () {
+		var $select = $(this).siblings('select');
+		var type = $select.data('type'); // from HTML data-type attribute
+		var id = $select.find(':selected').val();
+		// get associated attraction and add it to the current day in the trip
+		var attraction = attractionsModule.getByTypeAndId(type, id);
+		tripModule.addToCurrent(attraction);
+	});
 
 });
---
+++ routes/api/attractions.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = require('express').Router();
+
+var db = require('../../models/_db');
+var Hotel = require('../../models/hotel');
+var Restaurant = require('../../models/restaurant');
+var Activity = require('../../models/activity');
+
+
+
+
+
+router.get('/hotels', function(req, res, next) {
+	Hotel.findAll()
+		.then(function(hotels) {
+			res.send(hotels)	
+		});
+});
+
+router.get('/restaurants', function(req, res, next) {
+	Restaurant.findAll()
+		.then(function(restaurants) {
+			res.send(restaurants)	
+		});
+});
+
+router.get('/activities', function(req, res, next) {
+	Activity.findAll()
+		.then(function(activities) {
+			res.send(activities)	
+		});
+});
+
+
+
+// $.ajax({
+//   method: 'GET',
+//   url: '/api/restaurants'
+// })
+// .then(function (restaurants) {
+//   restaurants.forEach(function() {
+  	
+//   })
+// })
+// .catch(console.error.bind(console));
+
+
+
+// $.get('/api/restaurants')
+// .then(function (restaurants) {
+//   restaurants.forEach(function(restaurant){
+//     console.log(restaurant.name);
+//   });
+// })
+// .catch( console.error.bind(console) );
+
+
+
+
+
+
+module.exports = router;
+
